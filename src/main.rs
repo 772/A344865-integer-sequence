21f4efe751116b96f57d4f36639632759c0517a7
Armin S <arminnessie@gmail.com>
2022-09-05 21:52:36 +0200

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::collections::HashSet;
+
+fn a(n: usize, list_of_prime_gaps: &Vec::<usize>) -> usize {
+    let mut saved: HashSet<Vec::<usize>> = HashSet::new();
+    for i in 1..(list_of_prime_gaps.len()-n) {
+		let sequence = list_of_prime_gaps[i..i+n].to_vec();
+        if saved.contains(&sequence) {
+            return i + n - 1
+        }
+        saved.insert(sequence);
+    }
+    0
+}
+
+fn main() {
+	// a(15) = 10252040. a(16) is not in the first 50.000.000 prime gaps.
+	let primes: Vec<usize> = primal::Primes::all().take(10252043).collect();
+	let mut list_of_prime_gaps = Vec::<usize>::new();
+	for i in 1..primes.len() {
+		list_of_prime_gaps.push(primes[i] - primes[i-1]);
+	}
+	for n in 1..15 {
+		print!("{}, ", a(n, &list_of_prime_gaps));
+	}
+	println!("");
+}
